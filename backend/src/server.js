8a95b12a8dd5f69a4ff2d6972d6cf164ffbe2b9f
aakarshanbasubhardwaj <aakarshanbasubhardwaj@gmail.com>
2024-12-29 15:32:16 +0100

    docker

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors())
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get('/test', (req, res) => {
   res.send('Test successful');
