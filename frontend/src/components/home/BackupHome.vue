5414b48c9d37ddf950c4ed2e0cac85f0dc516af9
aakarshanbasubhardwaj <aakarshanbasubhardwaj@gmail.com>
2025-01-24 14:19:24 +0100

    collapsible drawer and visibility for small and medium screens

--- frontend/src/components/home/BackupHome.vue
+++ frontend/src/components/home/BackupHome.vue
@@ -1,6 +1,19 @@
 <template>
     <v-app>
-      <v-navigation-drawer app color="#06367a">
+      <v-app-bar
+        color="primary"
+        prominent
+        v-if="$vuetify.display.mdAndDown"
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>
+      <v-navigation-drawer 
+        app 
+        color="#06367a" 
+        v-model="drawer"
+        :location="$vuetify.display.mobile ? 'left' : 'left'"
+        
+        >
         <v-list>
           <v-list-item link :to="{ path: '/home' }" style="display: flex; align-items: center;">
             <v-avatar class="mx-auto mb-4" size="80" >
@@ -179,7 +192,14 @@
         filesToUpload: [],
         uploadStatus: null,
         uploadProgress: 0,
+        drawer: true,
+        group: null,
       };
+    },
+    watch: {
+      group () {
+        this.drawer = false
+      },
     },
     methods: {
       handleFileUpload(event) {
